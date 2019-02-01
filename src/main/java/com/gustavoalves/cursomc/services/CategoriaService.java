8724fc8d313e7729cb09f42d71e3f61f3314b101
gustavoasebba <gustavoasebba@yahoo.com.br>
2019-02-01 16:05:31 -0200

    Criando operacao de instaciacao

--- src/main/java/com/gustavoalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/gustavoalves/cursomc/services/CategoriaService.java
@@ -1,23 +1,19 @@
 package com.gustavoalves.cursomc.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
-import com.gustavoalves.cursomc.repositories.CategoriaRepository;
 import com.gustavoalves.cursomc.resources.domain.Categoria;
 
 @Service
 public class CategoriaService {
-	
+
 	public Categoria buscar() {
 		
-		@Autowired
-		private CategoriaRepository repo;
-		
-		public Categoria buscar(Integer id) {
-			Categoria obj = repo.findOne(id);
-		}
-		
+		public Categoria find(Integer id) { 
+			Optional<Categoria> obj = repo.findById(id); 
+			return obj.orElse(null); } 
 	}
 
 }
--- src/main/java/com/gustavoalves/workshopmongo/CursomcApplication.java
+++ src/main/java/com/gustavoalves/workshopmongo/CursomcApplication.java
@@ -3,11 +3,21 @@ package com.gustavoalves.workshopmongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.gustavoalves.cursomc.resources.domain.Categoria;
+
 @SpringBootApplication
 public class CursomcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception{
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
 	}
 
 }
