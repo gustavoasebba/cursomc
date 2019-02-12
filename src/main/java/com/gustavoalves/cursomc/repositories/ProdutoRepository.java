485a5038c1de04fc014521ae3b15b940f1de3b08
gustavoasebba <gustavoasebba@yahoo.com.br>
2019-02-12 10:56:54 -0200

    Produto e asociacao muitos para muitos

--- src/main/java/com/gustavoalves/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/gustavoalves/cursomc/repositories/ProdutoRepository.java
@@ -3,9 +3,9 @@ package com.gustavoalves.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.gustavoalves.cursomc.resources.domain.Produto;
+import com.gustavoalves.cursomc.resources.domain.Categoria;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Produto, Integer> {
+public interface ProdutoRepository extends JpaRepository<Categoria, Integer> {
 
 }
--- src/main/java/com/gustavoalves/cursomc/resources/domain/Produto.java
+++ src/main/java/com/gustavoalves/cursomc/resources/domain/Produto.java
@@ -8,51 +8,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preço;
 	
-	@ManyToMany(mappedBy="categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable
+	(name = "PRODUTO_CATEGORIA",
+	      joinColumns = @JoinColumn(name = "produto_id"),
+	      inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList();
 	
-	public Categoria() {		
-	}
-
-	public Categoria(Integer id, String nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
+	public Produto() {
 	}
 
 	public Integer getId() {
 		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public List<Produto> getProdutos() {
-		return produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
 	}
 
 	@Override
@@ -71,7 +53,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -80,7 +62,40 @@ public class Categoria implements Serializable {
 		return true;
 	}
 
-	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreço() {
+		return preço;
+	}
+
+	public void setPreço(Double preço) {
+		this.preço = preço;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+
+	public Produto(Integer id, String nome, Double preço) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preço = preço;
+	}
 	
 	
 
